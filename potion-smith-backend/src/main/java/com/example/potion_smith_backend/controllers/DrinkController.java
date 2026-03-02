95a5bf83003c9a7f27f8729d314ed1724b791427
PuiSlate <pkaraket73@gmail.com>
2026-03-01 22:14:30 -0600

    Refactored GetMapping both for getAllDrinks and getDrinkById with ResponseEntity object with HttpStatus 200 OK

--- potion-smith-backend/src/main/java/com/example/potion_smith_backend/controllers/DrinkController.java
+++ potion-smith-backend/src/main/java/com/example/potion_smith_backend/controllers/DrinkController.java
@@ -4,6 +4,7 @@ import com.example.potion_smith_backend.models.Drink;
 import com.example.potion_smith_backend.repositories.DrinkRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,38 +19,49 @@ public class DrinkController {
     DrinkRepository drinkRepository;
 
     //    Retrieve all drinks from database
+    // refactored to return a ResponseEntity object with an HttpStatus of 200 OK
 //    GET http://localhost:8080/api/drinks
     @GetMapping("")
-    public List<?> getAllDrinks() {
-        return drinkRepository.findAll();
+    public ResponseEntity<?> getAllDrinks() {
+        List<Drink> allDrinks = drinkRepository.findAll();
+        return new ResponseEntity<>(allDrinks, HttpStatus.OK); //200
     }
 
     //    Retrieve a specific drink object using its id
+//    refactored to return a ResponseEntity object with an HttpStatus of 200 OK
 //    GET http://localhost:8080/api/drinks/details/3 (for example)
     @GetMapping("/details/{drinkId}")
-    public Drink getDrinkById(@PathVariable int drinkId) {
-        return drinkRepository.findById(drinkId).orElse(null);
+    public ResponseEntity<?> getDrinkById(@PathVariable int drinkId) {
+        Drink drink = drinkRepository.findById(drinkId).orElse(null);
+        return new ResponseEntity<>(drink, HttpStatus.OK); // 200
     }
 
-    //    Save a new drink to the database
-//    refactored from RequesParams to a RESTful controller by mapping the base path to /api/drinks, used @RequestBody to accept JSON
+    //     Save a new drink to the database
+//    refactored from RequestParams to a RESTful controller by using @RequestBody to accept JSON
 //    for POST requests, persisted data using Spring Data JPA, and returned proper HTTP status codes using ResponseEntity.
-//    POST http://localhost:8080/api/drinks/
-
-    @PostMapping
-    public ResponseEntity<Drink> addNewDrink(@RequestBody Drink drink) {
-        Drink savedDrink = drinkRepository.save(drink);
-        return new ResponseEntity<>(savedDrink, HttpStatus.CREATED);
+//    POST http://localhost:8080/api/drinks/add
+//Ensure the mapping is configured to consume JSON instead of using query params
+    @PostMapping(value= "/add", consumes= MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> addNewDrink(@RequestBody Drink drink) {
+        drinkRepository.save(drink);
+        return new ResponseEntity<>(drink, HttpStatus.CREATED); //201
 
     }
 
-//    Update the existing drink in the database
-//    PUT http://localhost:8080/api/drinks/3 (for example)
 
 
-
-//    Delete a drink from the database
+    //    Delete a drink from the database
 //    DELETE http://localhost:8080/api/drinks/3 (for example)
+    @DeleteMapping("/{drinkId}")
+    public ResponseEntity<Void> deleteDrink(@PathVariable int drinkId) {
+        return drinkRepository.findById(drinkId)
+                .map(drink -> {
+                    drinkRepository.delete(drink);
+                    return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
+                })
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+
+    }
 }
 
 
---
+++ potion-smith-backend/src/main/java/com/example/potion_smith_backend/controllers/UserController.java
@@ -0,0 +1,4 @@
+package com.example.potion_smith_backend.controllers;
+
+public class UserController {
+}
